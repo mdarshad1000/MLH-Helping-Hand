32ffd6d63df2cf9f46f6a12d9ff2ed23e7fd4e4c
Mohammad Arshad <mdarshad1000@gmail.com>
2022-03-12 21:22:30 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,98 @@
+import pyrebase
+from flask import *
+
+app = Flask(__name__)
+config = {
+    "apiKey": "AIzaSyBs27uES8ItnzIdLKfcPRceB8cZxE2LyW8",
+    "authDomain": "helpinghand-290e9.firebaseapp.com",
+    "databaseURL": "https://helpinghand-290e9.firebaseio.com",
+    "projectId": "helpinghand-290e9",
+    "storageBucket": "helpinghand-290e9.appspot.com",
+    "messagingSenderId": "273535791016",
+    "appId": "1:273535791016:web:3755fc4ba69a3e6673379d",
+    "measurementId": "G-H7QW7W9BL8"
+}
+
+# INITIALIZE FIREBASE
+firebase = pyrebase.initialize_app(config)
+auth = firebase.auth()
+db = firebase.database()
+
+person = {"is_logged_in": False, "name": "", "email": "", "uid": ""}
+
+
+@app.route("/")
+def login():
+    return render_template("login.html")
+
+
+@app.route("/signup")
+def signup():
+    return render_template("signup.html")
+
+
+@app.route("/welcome")
+def welcome():
+    if person["is_logged_in"] == True:
+        return render_template("welcome.html", email=person["email"], name=person["name"])
+    else:
+        return redirect(url_for('login'))
+
+
+@app.route("/result", methods=["POST", "GET"])
+def result():
+    if request.method == "POST":
+        result = request.form
+        email = result["email"]
+        password = result["pass"]
+        try:
+            user = auth.sign_in_with_email_and_password(email, password)
+            global person
+            person["is_logged_in"] = True
+            person["email"] = user["email"]
+            person["uid"] = user["localId"]
+            data = db.child("users").get()
+            person["name"] = data.val()[person["uid"]]["name"]
+            return redirect(url_for('welcome'))
+        except:
+            return redirect(url_for('result'))
+    else:
+        if person["is_logged_in"] == True:
+            if request.method == 'POST':
+                if request.form.get('logout') == 'LOGOUT':
+                    auth.signOut()
+                    return redirect(url_for('login'))
+            return redirect(url_for('welcome'))
+        else:
+            return redirect(url_for('result'))
+
+
+@app.route("/register", methods=["POST", "GET"])
+def register():
+    if request.method == "POST":
+        result = request.form
+        email = result["email"]
+        password = result["pass"]
+        name = result["name"]
+        try:
+            auth.create_user_with_email_and_password(email, password)
+            user = auth.sign_in_with_email_and_password(email, password)
+            global person
+            person["is_logged_in"] = True
+            person["email"] = user["email"]
+            person["uid"] = user["localId"]
+            person["name"] = name
+            data = {"name": name, "email": email}
+            db.child("users").child(person["uid"]).set(data)
+            return redirect(url_for('welcome'))
+        except:
+            return redirect(url_for('register'))
+    else:
+        if person["is_logged_in"] == True:
+            return redirect(url_for('welcome'))
+        else:
+            return redirect(url_for('register'))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
